ef45e77f953fb2f46722deeb346e948524169d05
Rishabh Jain <rishabhj945@gmail.com>
2025-08-24 12:26:57 +0530

    added feeding records API

--- src/domains/medical_record/model.ts
+++ src/domains/medical_record/model.ts
@@ -20,8 +20,6 @@ export interface IMedicalRecord extends Document {
   follow_up_date?: Date;
   notes?: string;
   extra_attributes: Record<string, any>;
-  createdAt: Date;
-  updatedAt: Date;
 }
 
 const MedicalRecordSchema = new Schema<IMedicalRecord>({
